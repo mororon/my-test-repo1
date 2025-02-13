7678dc208b7d64eec164f0275ac6a80c2e0d98c1
師井 学 <moroiman@st.ritsumei.ac.jp>
2025-02-13 21:42:43 +0900

    誤記修正

--- go/excellent/main_test.go
+++ go/excellent/main_test.go
@@ -2,8 +2,8 @@ package main
 
 import "testing"
 
-funt TestEvenOrOdd(t *testing.T) {
-	resulet := EvenOrOdd(10)
+func TestEvenOrOdd(t *testing.T) {
+	result := EvenOrOdd(10)
 	if result != "even" {
 		t.Errorf("expected: even, actual: %s", result)
 	}
